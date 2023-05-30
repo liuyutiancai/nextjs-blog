69fc29cdd141fce4d3b453db9fa4ea971fc98ba6
liuyuu <1849528613@qq.com>
2023-05-30 14:41:55 +0800

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                </Head>
+                <h1>First Post</h1>
+                <h2>
+                    <Link href='/'>Back to home</Link>
+                    {/* <a href="/">Back to home</a> */}
+                </h2>
+            </Layout>
+            
+        </>
+        
+    )
+    
+}
