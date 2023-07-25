e38d1b6b0979a83ff15ab7673799c647052087f3
liuyuu <1849528613@qq.com>
2023-07-25 23:48:29 +0800

    main commit

--- pages/test.js
+++ pages/test.js
@@ -1,3 +1,8 @@
 export default function Test() {
-    return <h1>Test Page</h1>
+    // return <h1>Test Page</h1>
+    return (
+        <>
+            <h1>All Test Page</h1>
+        </>
+    )
 }
